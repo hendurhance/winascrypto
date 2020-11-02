74cc3ef3e1d17dd8626e20da3a006823b8a30b47
hendurhance <50846992+hendurhance@users.noreply.github.com>
2020-11-02 17:41:56 +0100

    another change

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -525,6 +525,16 @@
 <!--end swal alert message-->
 @yield('scripts')
 
-
+<script>
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5f9d52fa16ea1756a6df1823/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+   </script>
 </body>
 </html>
