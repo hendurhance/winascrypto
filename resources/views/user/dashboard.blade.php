4fac4f80d0334f1ec5a17e605f5a76936b445aa8
hendurhance <50846992+hendurhance@users.noreply.github.com>
2020-11-02 17:37:26 +0100

    final changes for the day

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -305,17 +305,6 @@
         /*new Clipboard('.has');*/
 
     </script>
-    <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/5f9d52fa16ea1756a6df1823/default';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
     <script>
         (function() {
 
